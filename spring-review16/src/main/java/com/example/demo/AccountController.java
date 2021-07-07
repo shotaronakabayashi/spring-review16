455e89d049a49cd202769dcf61b4a92303f1c047
id <id@IN0013453>
2021-07-07 16:56:17 +0900

    StoreControllerの作成

--- spring-review16/src/main/java/com/example/demo/AccountController.java
+++ spring-review16/src/main/java/com/example/demo/AccountController.java
@@ -60,34 +60,4 @@ public class AccountController {
 		return mv;
 	}
 
-
-
-
-	//店舗登録をクリックされた
-	@GetMapping("/addstore")
-	public ModelAndView addStore (ModelAndView mv) {
-
-
-		mv.setViewName("addstore");
-		return mv;
-	}
-
-
-	//店舗情報が入力されて登録ボタンが押された
-	@PostMapping("/addstore")
-	public ModelAndView addStore2 (ModelAndView mv) {
-
-
-		mv.setViewName("top");
-		return mv;
-	}
-
-
-
-
-
-
-
-
-
 }
--- spring-review16/src/main/java/com/example/demo/StoreController.java
+++ spring-review16/src/main/java/com/example/demo/StoreController.java
@@ -1,8 +1,27 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StoreController {
+
+	//店舗登録をクリックされた
+	@GetMapping("/addstore")
+	public ModelAndView addStore(ModelAndView mv) {
+
+		mv.setViewName("addstore");
+		return mv;
+	}
+
+	//店舗情報が入力されて登録ボタンが押された
+	@PostMapping("/addstore")
+	public ModelAndView addStore2(ModelAndView mv) {
+
+		mv.setViewName("top");
+		return mv;
+	}
 
 }
