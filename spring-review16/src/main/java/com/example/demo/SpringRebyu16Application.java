73a4f72c35d6cf2b8c5ecfe6dff68e8bb7a29bb2
id <id@IN0013453>
2021-07-07 13:48:56 +0900

    1st

---
+++ spring-review16/src/main/java/com/example/demo/SpringRebyu16Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRebyu16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRebyu16Application.class, args);
+	}
+
+}
